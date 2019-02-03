6f953cdc1da8932d06a4fddf606a91189bf1ce80
cr33dx <shashwatmahar12@gmail.com>
2019-02-03 17:37:30 +0530

    test2

--- test.js
+++ test.js
@@ -20,18 +20,28 @@ describe('test 1',()=>{
         done()
     })
 
-    test('check if email is incomplete',async (done)=>{
-        await page.type("input[name=username]", "hwllo")
+    test('check if email is complete',async (done)=>{
+        await page.type("input[name=username]", "hwllogmailm")
         await page.click('button[type=submit]')
-        await page.waitFor(4000)
         //let x = await page.waitFor('//*[@id="app"]/div/div/div[1]/section/div/div/div/div/div[2]/form/div[1]/p') <- xpath
         let x = await page.$('#app > div > div > div.new-login > section > div > div > div > div > div.login-frame > form > div:nth-child(1) > p')
         // To get text content use evaluate i.e same as eval 1st param is func 2nd param is args to be passed.
-        const text = await page.evaluate((x)=>x.textContent, x)
-        console.log(text)
-        expect(text).toEqual("Email is invalid")
+        //const text = await page.evaluate((x)=>x.textContent, x)
+        //console.log(text)
+        let flag = 0
+        if(x != null){flag = 1}
+        expect(flag).toEqual(0)  
         done()
 
+    })
+
+    test('check if password is not typed in', async(done)=>{
+        let flag = 0
+        let x = await page.$("#app > div > div > div.new-login > section > div > div > div > div > div.login-frame > form > div.md-form.info-icon.has-error > p")
+        await page.evaluate((x)=>console.log(x.textContent),x)
+        x != null?flag=1:null;
+        expect(flag).toEqual(0)
+        done()
     })
 })
 
